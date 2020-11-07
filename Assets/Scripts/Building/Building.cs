c03e7230afa044526c1d264b87989d82e5cbf6d6
vshaw18 <vshaw18@uw.edu>
2020-11-06 23:30:53 -0800

    1st Iteration.

---
+++ Assets/Scripts/Building/Building.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Building : MonoBehaviour
+{
+    private float health;
+    private Color defaultColor;
+    // Add resource type
+
+
+    public abstract void connectBuilding(GameObject build);
+    public abstract void disconnectBuilding(GameObject build);
+    public abstract void takeDamage(float damage);
+    public abstract float getHealth(); 
+    public abstract void receiveResource();
+    public abstract void sendResource();
+}
---
+++ Assets/Scripts/UI/ButtonBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    static private GameManager sGameManager = null;
+    static public void setGameManager(GameManager g) { sGameManager = g; }
+
+    private bool infoTab = false;
+
+    public void OnButtonPress()
+    {
+        infoTab = !infoTab;
+        sGameManager.activateInformationTab(infoTab);
+    }
+}
