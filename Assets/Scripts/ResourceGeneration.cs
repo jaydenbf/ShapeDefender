624aa9524c27d038e960bd9c43ddfec5a60ee972
SammyCLew <SammyCLew@icloud.com>
2020-11-07 00:46:06 -0800

    Procedural Generation
    Experimented with perlin noise generation.
    
    Enable it through a script in the main camera.

---
+++ Assets/Scripts/ResourceGeneration.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceGeneration : MonoBehaviour
+{
+    void Start()
+    {
+        int seed = UnityEngine.Random.Range(0, 1000000);
+
+        for (float x = -31.5f; x <= 31.5f; x++)
+        {
+            for (float y = -17.5f; y <= 17.5f; y++)
+            {
+                float k = Mathf.PerlinNoise((x + seed) / 10f, (y + seed) / 10f);
+
+                if (k > 0.5f)
+                {
+                    GameObject l = Resources.Load<GameObject>("Prefabs/Red Square") as GameObject;
+                    GameObject o = GameObject.Instantiate(l) as GameObject;
+                    o.transform.position = new Vector3(x, y, 0f);
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
