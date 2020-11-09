d31d754d0a4b3a0b456ef5151c5bbaceac4f9863
jaydenbf <47370644+jaydenbf@users.noreply.github.com>
2020-11-09 14:38:38 -0800

    Organization, Added buildings to mouse prototyope

--- Assets/Scripts/GameZone.cs
+++ Assets/Scripts/GameZone.cs
@@ -16,7 +16,7 @@ public class GameZone : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        circles[0] = (Tile)Resources.Load("Tilesets/Objects/red_circle", typeof(Tile));
+        /*circles[0] = (Tile)Resources.Load("Tilesets/Objects/red_circle", typeof(Tile));
         circles[1] = (Tile)Resources.Load("Tilesets/Objects/green_circle", typeof(Tile));
         circles[2] = (Tile)Resources.Load("Tilesets/Objects/blue_circle", typeof(Tile));
 
@@ -33,7 +33,7 @@ public class GameZone : MonoBehaviour
             int toRemove = Random.Range(0, localTilesPositions.Count - 1);
             baseLevel.SetTile(localTilesPositions[toRemove], circles[i]);
             localTilesPositions.RemoveAt(toRemove);
-        }
+        }*/
     }
 
     // Update is called once per frame
